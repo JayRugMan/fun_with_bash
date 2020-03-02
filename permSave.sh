888acf8e0302fd589f346b3b9b3f7e1e9004bf99
JayRugMan <jasonrhardman@gmail.com>
2020-03-02 16:21:51 -0700

    I corrected the limitation that prevented working with filenames with spaces and special characters I also found a way to use find and stat instead of a for-loop, drastically reducing the amount of code needed to create the permissions output file

--- permSave.sh
+++ permSave.sh
@@ -4,52 +4,18 @@
 # which the script is issued into a file that simply needs
 # to be sourced to restore permissions.
 # Limitation:
-#   - files or directories with spaces or special characters
-#     xnix systems recommend against, IE SPACE, <, >, |, :,
-#     (, ), &, ;, *, ? are not handled properly- errors will
-#     occur when sourcing the restore file.
 #   - It currently doesn't handle sticky-bits, only r, w,
 #     and e.
-# To check whether any filenames have characters that break this script,
-# issue the following first:
-# ls -R | grep -E "./[ <>|\:()$;?*]{1,}./"
- 
-function permfilter(){
-  ## This module takes a single permissions trio, ID r-x, or
-  ## rwx, etc. and prints the corresponding chmod numeric
-  ## mode number, 0-7
-  permissions="${1}"
-  loadModBit=0
-   
-  for i in `seq 0 2`; do
-    case ${permissions:$i:1} in
-      r)
-         loadModBit=$(($loadModBit+4));;
-      w)
-         loadModBit=$(($loadModBit+2));;
-      x)
-         loadModBit=$(($loadModBit+1));;
-    esac
-  done
-     
-    echo "${loadModBit}"
-}
+
+
 # Sets home directory from where the directory tree starts, To abstract absolute path
 homedir="$PWD"
-# Creates a directory tree with sub-directory titles and it's contents' permissions
-DirTreePerms="$(for i in `find . -type d`; do echo $i; ls -l $i | grep -v total; done)"
-sourcepermFile="/home/jhardman/dokuwikiPerm_${homedir##*/}_$(date +%y%m%d_%H%M).file" # file for sourcing
-#JH sourcepermFile=/home/jhardman/testPerm.file # file for sourcing
-echo "$DirTreePerms" | awk '{print $1" "$9}' | \
-while read perms object; do
-  if [[ -z $object ]]; then # this basically means the line printed is the subdirectory itself - basically,
-                            # the object is in the perms argument
-    subdir="$(echo "$homedir/${perms}" | sed 's/\.\///g')" # appends the subdirectory to the base directory
-    ##JH echo $subdir
-    ##JH echo "cd $subdir" | sh
-    continue
-  else
-    echo "chmod $(permfilter ${perms:1:3})$(permfilter ${perms:4:3})$(permfilter ${perms:7:3}) ${subdir}/${object} > /dev/null 2>&1" >> ${sourcepermFile}
-  fi
-done
+### MODIFY TARGET DIRECTORY FOR PERMISSION RESTORE FILE ###
+sourcepermFile="$HOME/${homedir##*/}_$(date +%y%m%d_%H%M).file" # file for sourcing
+
+# Finds all objects and checks the stats, echoing the permission number bits
+# in context of the chmod command and directs the constructed chmod command
+# to the sourcepermFile to be sourced to restore permissions
+find . -exec stat -c"chmod %a \"$homedir/%n\" 2>/dev/null" {} + | sed 's/\/\.//g' > ${sourcepermFile}
+
 echo -e "\nsource $sourcepermFile as root to restore mod bits for files in $homedir\n"
