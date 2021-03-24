15f3b421d54b0f8cea190a7aa04719b668381f2c
jayrugman <jasonrhardman@gmail.com>
2021-03-23 19:55:08 -0600

    media_backup.sh modified for more compact movie list log output

--- media_backup.sh
+++ media_backup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# This script is for backing up media from the /home/plex directory
-# the idea is to have it check weekly which files have been added 
-# the last tarball was creted, then create a new tarball with only
-# the new movies included
+# This script is for backing up media from the a source directory.
+# The idea is to have it check weekly which files have been added 
+# since the last tarball was creted, then create a new tarball with 
+# only the new files included.
 
 PATH=/usr/local/sbin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin
 
@@ -12,13 +12,16 @@ THEDATE=$(date +%Y%m%d-%H%M)
 
 SOURCE_DIR="/home/plex"
 TARGET_DIR="/mnt/backups"
-ARCIVE_FILE="$TARGET_DIR/media_backups_plex01_$THEDATE.tgz"
+ARCHIVE_FILE="$TARGET_DIR/media_backups_plex01_$THEDATE.tgz"
 LOGFILE="/mnt/backups/media_bu.log"
 
 ### End Customization ###
 
+
+# creates log file if it does not exist already
 if [[ ! -f $LOGFILE ]] ; then touch $LOGFILE; fi
 
+# Get's the last archive file for time comparison in find command
 LATEST_BU="$(ls -1t $TARGET_DIR | grep media_backups_plex01 | head -1)"
 
 
@@ -41,12 +44,12 @@ function main() {
     exit 1
   else
     echo "$THEDATE:  Starting Backup" >> $LOGFILE
-    echo "tar czf $ARCIVE_FILE $filesString" | /bin/sh
+    echo "tar czf $ARCHIVE_FILE $filesString" | /bin/sh
     if [[ $? -eq 0 ]]; then
-      echo "successfully archived the following files into $ARCIVE_FILE:" >> $LOGFILE
-      printf "$(echo "$filesString" | sed 's/^"/\\n/g;s/" "/\\n/g;s/"$/\\n/g')\n\n" >> $LOGFILE
+      echo "successfully archived the following files into $ARCHIVE_FILE:" >> $LOGFILE
+      printf "$(echo "$filesString" | sed 's/^"//g;s/" "/\\n/g;s/"//g')\n\n" >> $LOGFILE
     else
-      echo "Backup failed"
+      echo "Backup failed" >> $LOGFILE
     fi
   fi
 }
