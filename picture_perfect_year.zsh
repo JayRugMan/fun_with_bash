95ff5db6385ea77cc5866de49ffe0fe05e9d6936
jayrugman <jasonrhardman@gmail.com>
2021-11-04 11:31:59 -0600

    made picture_perfect_year.zsh work with movies

--- picture_perfect_year.zsh
+++ picture_perfect_year.zsh
@@ -8,20 +8,36 @@
 
 function get_pic_info() {
   # loads PICTURES array with picture name and date as key and value
-  for the_pic in $(ls | grep -i "jpg\|png"); do
-    pic_type=${the_pic##*.}
+  # the reason I don't just pull the creation date from identify on all images
+  # is because with jpg files, the taken date in the exif data is older and
+  # likely more accurate. However, if the exif data is not available on a jpg,
+  # then identify is tried. If both fail, then "unknown_date" is assigned
+  # This function can also handle video files.
+  IFS=$'\n'
+  for the_pic in $(ls | grep -i "jpg\|jpeg\|png\|gif\|mp3\|mp4\|wav\|mov"); do
+    pic_type="${the_pic##*.}"
     if [[ ${pic_type:l} == "jpg" ]] || [[ ${pic_type:l} == "jpeg" ]]; then
-      the_y=$(exif -t DateTimeOriginal --machine-readable $the_pic |
+      the_y="$(exif -t DateTimeOriginal --machine-readable "$the_pic" 2>/dev/null |
                awk -F':' '{print $1}'
-              )
-      PICTURES[$the_pic]="$the_y"
-    elif [[ ${pic_type:l} == "png" ]]; then
-      the_y=$(identify -verbose $the_pic |
-               awk -F'( |-){1,}' '/date:create:/ {print $3}'
-              )
-      PICTURES[$the_pic]="$the_y"
+               )"
     fi
+    if [[ ${pic_type:l} == "mp3" ]] || [[ ${pic_type:l} == "mp4" ]] || [[ ${pic_type:l} == "wav" ]] || [[ ${pic_type:l} == "mov" ]] ; then
+      the_y="$(ffprobe -hide_banner "$the_pic" 2>&1 |
+               awk -F'( |-){1,}' '/creation_time/ || / date / {print $4; exit}'
+              )"
+    fi
+    if [[ ${pic_type:l} == "png" ]] || [[ ${pic_type:l} == "gif" ]] || [[ -z "$the_y" ]]; then
+      the_y="$(identify -verbose "$the_pic" 2>/dev/null |
+               awk -F'( |-){1,}' '/date:create:/ {print $3}'
+              )"
+    fi
+    if [[ -z "$the_y" ]]; then
+      the_y="unknown_date"
+    fi
+    PICTURES[$the_pic]="$the_y"
+    unset the_y
   done
+  unset IFS
 }
 
 
@@ -48,7 +64,7 @@ function make_dir() {
 function move_them_pics() {
   # Pictures are moved to the corresponding year directory
   for the_picture the_year in ${(@kv)PICTURES}; do
-    mv -v "$the_picture" "$the_year/"
+    echo "mv -v \"$the_picture\" \"$the_year\"" | /usr/bin/zsh
   done
 }
 
