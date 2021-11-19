3ea5b3d3b16c92741ca3b21085b04cc8aea934ef
jayrugman <jasonrhardman@gmail.com>
2021-11-19 12:57:26 -0700

    dirPermSave.sh now cd's to base directory

--- dirPermSave.sh
+++ dirPermSave.sh
@@ -10,7 +10,7 @@
 function usage() {
   # Prints usage
   if [[ ! -z ${1} ]]; then
-    echo "ERROR - ${1}"
+    echo "ERROR - ${@}"
   fi
   cat <<EOF
 
@@ -44,10 +44,15 @@ the_date_time="$(date +%Y%m%d-%H%M%S)"  # date/time for file name YYYYMMDD-HHMMS
 the_output_file="/tmp/dir_permissions_backup_${the_date_time}.txt"  # output file
 ##
 
+
   # Creates file by loading with instructive header
-echo "## source this file in \"${base_dir}\" to recover permissions and ownership" > ${the_output_file}
-  # finds directories in the base directory, skipping .snapshot directories, and loads output file with current permissions
-find "${base_dir}" -maxdepth $dir_depth -mindepth 1 -name .snapshot -prune -o -type d -exec stat -c"chown %U:%G %n" {} \; -exec stat -c"chmod %a %n" {} \; >> ${the_output_file}
-  # sed to add 00 before permissions without special permissions bit so any special bit can be removed when restored from backup
-sed -i 's/^chmod [0-7][0-7][0-7] /00&/g; s/^00chmod /chmod 00/g' ${the_output_file}
-echo -e "-- Rollback source file is ${the_output_file}\n--- (Source in directory specified on the file's first line)"
+if cd ${base_dir}; then
+  echo "## source this file in \"${base_dir}\" to recover permissions and ownership" > ${the_output_file}
+    # finds directories in the base directory, skipping .snapshot directories, and loads output file with current permissions
+  find "${base_dir}" -maxdepth $dir_depth -mindepth 1 -name .snapshot -prune -o -type d -exec stat -c"chown %U:%G %n" {} \; -exec stat -c"chmod %a %n" {} \; >> ${the_output_file}
+    # sed to add 00 before permissions without special permissions bit so any special bit can be removed when restored from backup
+  sed -i 's/^chmod [0-7][0-7][0-7] /00&/g; s/^00chmod /chmod 00/g' ${the_output_file}
+  echo -e "-- Rollback source file is ${the_output_file}\n--- (Source in directory specified on the file's first line)"
+else
+  usage "Could not change to ${base_dir}"
+fi
