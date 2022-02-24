331a396e877024e1df01c8a07c0aff2c660b13ee
jayrugman <jasonrhardman@gmail.com>
2022-02-24 15:56:00 -0700

    improved handling of instances where your total capacity is less than the spcified tar capacity limit

--- even_archiver.sh
+++ even_archiver.sh
@@ -18,19 +18,18 @@
 function get_files() {
   # Uses crafted find command to get all of the files
   IFS=$'\n'
-  THE_FILES=( $(find . -type f -exec du -sb {} \;| sort -n| sed 's/\t/ /g') )
+  THE_FILES=( $(find . -type f -exec du -sb {} \; 2>/dev/null| sort -n| sed 's/\t/ /g') )
   unset IFS
 }
 
 
 function calc_tot_capacity() {
   # Calculates the total, uncompressed capacity of the files to be archived
-  capacities=(${@})
   total=0
-  for i in ${capacities[@]}; do
+  for i in ${FILE_SIZES[@]}; do
     total=$((total + i))
   done
-  echo $total && unset total capacities
+  echo $total && unset total
 }
 
 
@@ -38,17 +37,20 @@ function calc_archv_count() {
   # This takes the number of files and tar size limit
   # to determine the number of final tar files
 
+  ##JH tar_size_limit=536870912  # 512M
   tar_size_limit=21474836480  # 20G
   ##JH tar_size_limit=10737418240  # 10G
   t_cap=$1
   tar_file_count=$((t_cap / tar_size_limit))
 
-  # if the capacity doesn't divide evenly by 10G and there
-  # is more than 3G left, or if the total capacityu is less
-  # than 10G, it adds another tar file to the count
-  ##JH if [[ $((t_cap % tar_size_limit)) -gt 3221225472 ]] || [[ "$tar_file_count" -eq 0 ]]; then
-  ##JH   ((tar_file_count++))
-  ##JH fi
+  # if the total capacity is less than the tar_size_limit, then tar_file_count
+  # will be 0, which won't work. This makes it at least one. Additionally, if
+  # there is just shy of two, the arithmatic will round down to 1, meaning an
+  # archive could end up with just shy of 40G. To avoid that, 1 becomes 2.
+  if [[ "$tar_file_count" -le 1 ]]; then
+    ((tar_file_count++))
+  fi
+  
   echo $tar_file_count
   unset tar_size_limit t_cap tar_file_count
 }
@@ -61,7 +63,7 @@ function deal_me_in() {
   for hand in ${@}; do
     ARCH_LISTS[$hand]+="\"${FILE_NAMES[$h_card]}\" \"${FILE_NAMES[$l_card]}\" "
   done
-  unset h_card t_card
+  unset h_card t_card hand
 }
 
 
@@ -69,55 +71,67 @@ function build_lists() {
   # builds lists for tar commands
 
   file_count=${#THE_FILES[@]}
-  half_file_count=$((file_count/2))
-  high_card=$((file_count - 1))
-  low_card=0
-  total_cap=$(calc_tot_capacity ${FILE_SIZES[@]})
+  total_cap=$(calc_tot_capacity)
   arch_file_count=$(calc_archv_count $total_cap)
-  twice_afc=$((arch_file_count * 2))
-  direction="right"
+    
+  if [[ "$arch_file_count" -gt 1 ]]; then
+    
+    half_file_count=$((file_count/2))
+    high_card=$((file_count - 1))
+    low_card=0
+    twice_afc=$((arch_file_count * 2))
+    direction="right"
+    
+    while [[ "$low_card" -lt "$half_file_count" ]]; do
+    
+      hi_lo_diff=$((high_card - low_card))
+      if [[ "${hi_lo_diff}" -lt "$twice_afc" ]]; then
+        break
+      fi
+    
+      case "$direction" in
+        "right")
+          deal_order=($(seq 1 "$arch_file_count"))
+          deal_me_in "$high_card" "$low_card" ${deal_order[@]}
+          direction="left"
+          ;;
+        "left")
+          deal_order=($(seq "$arch_file_count" -1 1))
+          deal_me_in "$high_card" "$low_card" ${deal_order[@]}
+          direction="right"
+          ;;
+      esac
+    
+      high_card=$((high_card - arch_file_count))
+      low_card=$((low_card + arch_file_count))
+    
+    done
+    
+    last_hand=$((arch_file_count + 1))
+    for remainders in $(seq 0 "$hi_lo_diff"); do
+      if [[ -z "${ARCH_LISTS[$last_hand]}" ]]; then
+        ARCH_LISTS[$last_hand]="\"${FILE_NAMES[$low_card]}\" "
+      else
+        ARCH_LISTS[$last_hand]+="\"${FILE_NAMES[$low_card]}\" "
+      fi
+      ((low_card++))
+    done
+  
+  else
+  
+    for file_index in ${!FILE_NAMES[@]}; do
+      ARCH_LISTS[1]+="\"${FILE_NAMES[${file_index}]}\" "
+    done
 
-  while [[ "$low_card" -lt "$half_file_count" ]]; do
-
-    hi_lo_diff=$((high_card - low_card))
-    if [[ "${hi_lo_diff}" -lt "$twice_afc" ]]; then
-      break
-    fi
-
-    case "$direction" in
-      "right")
-        deal_order=($(seq 1 "$arch_file_count"))
-        deal_me_in "$high_card" "$low_card" ${deal_order[@]}
-        direction="left"
-        ;;
-      "left")
-        deal_order=($(seq "$arch_file_count" -1 1))
-        deal_me_in "$high_card" "$low_card" ${deal_order[@]}
-        direction="right"
-        ;;
-    esac
-
-    high_card=$((high_card - arch_file_count))
-    low_card=$((low_card + arch_file_count))
-
-  done
-
-  last_hand=$((arch_file_count + 1))
-  for remainders in $(seq 0 "$hi_lo_diff"); do
-    if [[ -z "${ARCH_LISTS[$last_hand]}" ]]; then
-      ARCH_LISTS[$last_hand]="\"${FILE_NAMES[$low_card]}\" "
-    else
-      ARCH_LISTS[$last_hand]+="\"${FILE_NAMES[$low_card]}\" "
-    fi
-    ((low_card++))
-  done
+  fi
 }
 
 
 function and_now_archive() {
   # Archives
+  the_file="${1}"
   for i in ${!ARCH_LISTS[@]}; do
-    echo "tar cvzf /mnt/backups/media_backups_plex01_$(date +%Y%m%d_%H%M)_init_Movies-${i}.tgz ${ARCH_LISTS[$i]}" | sh
+    echo "tar cvzf ${the_file}${i}.tgz ${ARCH_LISTS[$i]}" | sh
     echo -e "\n"
   done
 }
@@ -129,9 +143,11 @@ function main() {
   declare -A ARCH_LISTS
   FILE_SIZES=(${THE_FILES[@]%% *})
   FILE_NAMES=("${THE_FILES[@]#* }")
+  target_base="/mnt/backups"
+  target_file="${target_base}/media_backups_plex01_$(date +%Y%m%d_%H%M)_init_Movies-"
   ####
   build_lists
-  and_now_archive
+  and_now_archive "${target_file}"
 }
 
 main
