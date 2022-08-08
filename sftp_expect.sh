8156eba1513c9de31df1607135b3e91e00f7acc7
jayrugman <jasonrhardman@gmail.com>
2022-08-08 16:45:50 -0600

    modified error handling for bad file name

--- sftp_expect.sh
+++ sftp_expect.sh
@@ -4,22 +4,25 @@ arg_count=$#
 
 ssh_user=${1}; shift
 ssh_host=${1}; shift
+remote_dir=${1}; shift
 ssh_port=${1}; shift
 ssh_key=${1}; shift
 ssh_ppf=${1}; shift
 
 case $ssh_key in
   "/home/jason/.ssh/id_rsa" ) 
-    expected_args=5
+    expected_args=6
     source "${ssh_ppf}"
     ##JH pp="1234wrongpw"
     ;;
-  "/home/jason/.ssh/id_rsa_2" ) expected_args=4;;
+  "/home/jason/.ssh/id_rsa_2" )
+    expected_args=5
+    pp="none";;
 esac
 
 function usage() {
   if [ ${1} -ne ${2} ] ; then
-    echo "Usage: ${0} user host port keyfile [passfile]"
+    echo "Usage: ${0} user host remote_dir port keyfile [passfile]"
     exit 1
   fi
 }
@@ -27,23 +30,22 @@ function usage() {
 usage $arg_count $expected_args
 
 expect << EOF
-##JH  spawn ssh -p ${ssh_port} ${ssh_user}@${ssh_host} "touch /home/jason/Desktop/test.file_$(date +%Y%m%d_%H%M%S)"
-  spawn sftp -oIdentityFile=${ssh_key} -P ${ssh_port} ${ssh_user}@${ssh_host}
-##JH  expect "Enter passphrase"
+  spawn sftp -oIdentityFile=${ssh_key} -P ${ssh_port} ${ssh_user}@${ssh_host}:${remote_dir}
+  set timeout 2
+  expect "Enter passphrase" {send "${pp}\r"}
 ##JH  send "${pp}\r"
-##JH  expect "sftp>"
-##JH  send "put test*.txt\rquit\r"
-##JH  expect eof
   expect {
-    "Enter passphrase" {send "${pp}\r"; exp_continue}
-    "Permission denied" {catch wait result; send_user "invalid password or account\n"; exit [lindex \$result 3]}
-    "Connection refused" {catch wait result; send_user "Connection to ${ssh_user}@${ssh_host} via port ${ssh_port} failed\n"; exit [lindex \$result 3]}
-    timeout {catch wait result; send_user "connection to ${ssh_host} timed out\n"; exit [lindex \$result 3]}
-    "sftp>" {send "put test*.txt\rquit\r"; exp_continue}
-    eof {catch wait result; exit [lindex \$result 3]}
+    "Permission denied" {send_user "invalid password or account\n"; exit 1}
+    "Connection refused" {send_user "Connection to ${ssh_user}@${ssh_host} via port ${ssh_port} failed\n"; exit 1}
+    "Connected" {exp_continue}
+    "Changing" {exp_continue}
   }
-##JH    timeout {send_user "connection to ${ssh_host} timed out\n"; exit}
-##JH    eof {exit}
+  expect "sftp"
+  send "put test*.txt\rquit\r"
+  expect {
+    "No such file or directory" {send "quit\r"; send_user "the file was not found.\n"; exit 1}
+    eof {exit}
+  }
 EOF
 exp_exit_code=$?
 
