a21c0f841e3a2ad1f9f4793803cffad2e66ddf1d
jayrugman <25958133+JayRugMan@users.noreply.github.com>
2025-01-29 01:48:19 -0700

    added brave window wrapper and modified to be agnostic with monitor hardware names. also added fix_brave.sh

--- brave_window_wrapper.sh
+++ brave_window_wrapper.sh
@@ -4,15 +4,15 @@
 eval $(xdotool getmouselocation --shell)
 
 # Get display information
-eval $(xrandr --query | awk 'BEGIN{FS="( | primary |x|+)"};/ connected/ {print $1 "=" $5}')
+eval $(xrandr --query | awk 'BEGIN{FS="( | primary |x|+)";screen_num=0};/ connected/ {print "scrn" screen_num "=" $1 ;print "scrn_addr" screen_num "=" $5; screen_num++}')
 
 # Determine which screen the mouse is on
-if [[ $X -lt $Virtual2 ]]; then
-    my_screen=Virtual1
-elif [[ $X -lt $Virtual3 ]]; then
-    my_screen=Virtual2
+if [[ $X -lt $scrn_addr1 ]]; then
+    my_screen="${scrn0}"
+elif [[ $X -lt $scrn_addr2 ]]; then
+    my_screen="${scrn1}"
 else
-    my_screen=Virtual3
+    my_screen="${scrn2}"
 fi
 
 ##JHecho "Mouse is on screen: $my_screen"
@@ -24,21 +24,20 @@ fi
 sleep 2
 
 # Get the window ID of the most recently opened Brave window
-WINDOW_ID=$(wmctrl -l | grep "Brave" | tail -1 | awk '{print $1}')
+WINDOW_ID=$(wmctrl -l | awk 'BEGIN{found}; /Brave/ {found=$1}; END{print found}')
 ##JHecho "Window ID: $WINDOW_ID"
 
 # Unmaximize the window
 wmctrl -ir $WINDOW_ID -b remove,maximized_vert,maximized_horz
 
 # Move the window to the correct screen
-if [[ $my_screen == "Virtual1" ]]; then
+if [[ "$my_screen" == "${scrn0}" ]]; then
     wmctrl -ir $WINDOW_ID -e 0,0,0,-1,-1
-elif [[ $my_screen == "Virtual2" ]]; then
-    wmctrl -ir $WINDOW_ID -e 0,$Virtual2,0,-1,-1
+elif [[ "$my_screen" == "${scrn1}" ]]; then
+    wmctrl -ir $WINDOW_ID -e 0,${scrn_addr1},0,-1,-1
 else
-    wmctrl -ir $WINDOW_ID -e 0,$Virtual3,0,-1,-1
+    wmctrl -ir $WINDOW_ID -e 0,${scrn_addr2},0,-1,-1
 fi
 
 # Maximize the window
 wmctrl -ir $WINDOW_ID -b add,maximized_vert,maximized_horz
-
--- fix_brave.sh
+++ fix_brave.sh
@@ -2,5 +2,4 @@
 # This script will unlink /usr/bin/brave-browser from /etc/alternatives/brave-browser
 sudo unlink /usr/bin/brave-browser
 # And link /usr/bin/brave-browser with /home/jasonhardman/bin/brave_window_wrapper.sh, which calls /etc/alternatives/brave-browser and other things
-sudo ln -s /home/jasonhardman/bin/brave_window_wrapper.sh /usr/bin/brave-browser
-
+sudo ln -s /home/jason/Documents/CodingProjects/fun_with_bash/brave_window_wrapper.sh /usr/bin/brave-browser
